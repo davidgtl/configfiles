2c9fbab10b456928674441f1e35a5771cb163d88
davidgtl <david.ghitulescu@gmail.com>
2019-11-06 21:21:47 +0200

    Setup script template -- not stable

---
+++ .setup.sh
@@ -0,0 +1,37 @@
+#/user/bin/env bash
+#nvidia drivers
+#https://askubuntu.com/questions/760934/graphics-issues-after-while-installing-ubuntu-16-04-16-10-with-nvidia-graphics
+sudo add-apt-repository ppa:graphics-drivers/ppa
+sudo apt-get update
+sudo ubuntu-drivers autoinstall
+
+#zsh goodies
+sudo apt install zsh
+cd $HOME
+git init
+git remote add origin https://github.com/davidgtl/configfiles.git
+git fetch --all
+git reset --hard
+cd .oh-my-zsh
+git init
+git remote add origin https://github.com/robbyrussell/oh-my-zsh.git
+git pull
+
+source $HOME/.zshrc
+
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+#vim goodies
+sudo apt install vim
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim +PluginInstall +qall
+
+#gnome
+sudo apt update && sudo apt install gnome-session
+sudo apt install gnome-tweaks
+sudo apt-get install blueman
+gsettings set org.gnome.desktop.wm.preferences button-layout 'close,maximize,minimize:'
+
+sudo apt-get build-dep xserver-xorg-input-mtrack
+https://howchoo.com/g/mdy0ngziogm/the-perfect-almost-touchpad-settings-on-linux-2
